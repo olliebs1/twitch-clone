a5988694ad7395ebee5bf94ef5dd24479259a9a2
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-11-05 16:00:47 +0000

    header component created with three initial dis

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header_left">
+                
+            </div>
+            <div className="header_center">
+
+            </div>
+            <div className="header_right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
