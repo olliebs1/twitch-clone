fdde284dfe2f3808e801f5e702e827ec371065ea
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-11-09 18:11:30 +0000

    Channel.js created and imported int sidebar to make up Channel component for followed and recommended channels

---
+++ src/Channel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Channel.css'
+
+const Channel = () => {
+    return (
+        <div className='channel'>
+            <div className='channel_details'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Channel
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Sidebar.css';
+import Channel from './Channel'
 
 const Sidebar = () => {
     return (
-        <div>
-            
+        <div className='sidebar'>
+            <h5>FOLLOWED CHANNELS</h5>
+            <Channel avatar=''
+                name='Olivier Benson-Smith'
+                followers='10k' />
+            <Channel avatar=''
+                name='Progamming'
+                followers='100k' />
+
+            <h5>RECOMMENDED CHANNELS</h5>
+            <Channel avatar=''
+                name='Recommended'
+                followers='1000' />
+            <p className='sidebar_topShowMore' >Show More</p>
         </div>
     )
 }
