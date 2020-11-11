7016534bffeb68ec078a1d9e198ec2f3873cfed8
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-11-11 12:47:37 +0000

    css styling finished for stream component and has been imported into body.js - project finished

--- src/Body.js
+++ src/Body.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Profile from './Profile'
 import './Body.css'
+import Stream from './Stream'
 
 const Body = () => {
     return (
         <div className='body'>
             <div className='body_left'>
+                <Stream />
                 <Profile />
             </div>
             <div className='body_rightPlaceHolder'>
-
             </div>
             
         </div>
--- src/Stream.js
+++ src/Stream.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Stream.css'
 
 const Stream = () => {
     return (
